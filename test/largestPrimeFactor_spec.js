6cc65cd994592a471d63599bee72322c22f61ca1
Jay Won <jason@sudokrew.com>
2015-05-24 11:11:28 -1000

    Updated name from challenge to module for concept purposes.

--- test/largestPrimeFactor_spec.js
+++ test/largestPrimeFactor_spec.js
@@ -1,16 +1,16 @@
-var challenge_3 = require("../largestPrimeFactor.js");
+var primeModule = require("../largestPrimeFactor.js");
 var chai = require('chai');
 var should = chai.should();
 
 describe("The largest Prime Factor", function () {
 
   it("for 13195 should be 29", function () {
-    var primeNumber = challenge_3.largestPrimeFactor(13195);
+    var primeNumber = primeModule.largestPrimeFactor(13195);
     primeNumber.should.equal(29);
   });
 
   it("for 600851475143 should be 6857", function () {
-    var primeNumber = challenge_3.largestPrimeFactor(600851475143);
+    var primeNumber = primeModule.largestPrimeFactor(600851475143);
     primeNumber.should.equal(6857);
   });
 });
