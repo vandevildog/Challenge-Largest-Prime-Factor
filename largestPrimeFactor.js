b0840dd0a3a2a2f721cb067c8b123283dd2d1c42
phi nguyen <vandevildog@gmail.comgit config --global user.email vandevildog@gmail.comgit commit -m 1st>
2019-07-25 18:15:00 -1000

    finished

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,27 @@
 exports.largestPrimeFactor = function(n) {
   let primeNumber = 0;
   // do your work here
+  let primeFactors = [];
+  while (n % 2 === 0){
+    primeFactors.push(2);
+    n /= 2;
+  }
+
+  const sqrtNum = Math.sqrt(n);
+  for (let i = 3; i <= sqrtNum; i++) {
+      while (n % i === 0) {
+          primeFactors.push(i);
+          n /= i;
+      }
+  }
+
+  if (n > 2){
+    primeFactors.push(num);
+  }
+
+  primeNumber = primeFactors[primeFactors.length - 1];
 
   return primeNumber;
 };
+
+
